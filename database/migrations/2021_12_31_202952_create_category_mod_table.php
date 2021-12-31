365f2c86e740216b02b941d931124eafac615c42
Isaac Dagel <isaac@isaacdagel.com>
2021-12-31 16:20:57 -0700

    Rename table

--- database/migrations/2021_12_31_202952_create_category_mod_table.php
+++ database/migrations/2021_12_31_202952_create_category_mod_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateModCategoryTable extends Migration
+class CreateCategoryModTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateModCategoryTable extends Migration
      */
     public function up()
     {
-        Schema::create('mod_category', function (Blueprint $table) {
+        Schema::create('category_mod', function (Blueprint $table) {
             $table->foreignId('mod_id');
             $table->foreignId('category_id');
             $table->primary(['mod_id', 'category_id']);
@@ -27,6 +27,6 @@ class CreateModCategoryTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mod_category');
+        Schema::dropIfExists('category_mod');
     }
 }
